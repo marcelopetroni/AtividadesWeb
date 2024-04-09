81fd6a8a280fc757f5614d158c3dca088fae48de
Marcelo Petroni <105806830+marcelopetroni@users.noreply.github.com>
2024-04-09 11:23:01 +0000

    mudanças

--- exercicioVetor.js
+++ exercicioVetor.js
@@ -2,11 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const global = [];
     const armazenarValores = document.querySelector('.forms-container');
     const campo = document.querySelector('.input');
+    const exibicao = document.querySelector('.output');
 
     armazenarValores.addEventListener('submit', function (e) {
         e.preventDefault();
         global.push(campo.value);
         campo.value = ''
         console.log(global);
+        exibir()
     });
+
+    const exibir = () => {
+        for (let index = 0; index < exibicao.length; index++) {
+            exibicao.innerText = global[i];
+        }
+    }
 });
