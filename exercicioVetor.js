478341c99272e6640421e062a6066bcfd93e77d9
marcelopetroni <marceloapetroni@hotmail.com>
2024-04-09 19:57:55 -0300

    bolinha de cores

--- exercicioVetor.js
+++ exercicioVetor.js
@@ -1,25 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
     const global = [];
+    const historicoCores = []
     const armazenarValores = document.querySelector('.forms-container');
     const campo = document.querySelector('.input');
     const exibicao = document.querySelector('.output-container');
+    const color = document.querySelector('.color-picker');
 
     armazenarValores.addEventListener('submit', function (e) {
         e.preventDefault();
         global.push(campo.value);
+        historicoCores.push(color.value)
         campo.value = ''
-        console.log(global);
         exibir()
     });
 
     const exibir = () => {
         exibicao.innerHTML = '';
         
-        for (let i = 0; i < global.length; i++) {
+        for (i = 0; i < global.length; i++) {
 
             const values = document.createElement('div');
-            values.innerText = global[i];
+            const numeracao = document.createElement('span')
+            const texto = document.createTextNode(global[i])
+
+            numeracao.innerText = (i + 1)
+            numeracao.style.backgroundColor = historicoCores[i]
+
+            values.appendChild(numeracao)
+            values.appendChild(texto)
+
             exibicao.appendChild(values);
         }
     }
+    
 });
